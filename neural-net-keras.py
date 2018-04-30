a9585a5f6e3aca2fc15d6c693b6cce975033d6b3
Keval Khara <kevalkhara95@gmail.com>
2018-04-29 20:51:12 -0400

    Neural Network using Keras

---
+++ neural-net-keras.py
@@ -0,0 +1,55 @@
+from getEmbeddings import getEmbeddings
+import numpy as np
+import keras
+from keras import backend as K
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, Embedding, Input, RepeatVector
+from keras.optimizers import SGD
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+
+xtr,xte,ytr,yte = getEmbeddings("datasets/train.csv")
+np.save('./xtr',xtr)
+np.save('./xte',xte)
+np.save('./ytr',ytr)
+np.save('./yte',yte)
+
+xtr = np.load('./xtr.npy')
+xte = np.load('./xte.npy')
+ytr = np.load('./ytr.npy')
+yte = np.load('./yte.npy')
+
+
+def baseline_model():
+    '''Neural network with 3 hidden layers'''
+    model = Sequential()
+    model.add(Dense(256, input_dim=300, activation='relu', kernel_initializer='normal'))
+    model.add(Dropout(0.3))
+    model.add(Dense(256, activation='relu', kernel_initializer='normal'))
+    model.add(Dropout(0.5))
+    model.add(Dense(80, activation='relu', kernel_initializer='normal'))
+    model.add(Dense(2, activation="softmax", kernel_initializer='normal'))
+
+    # gradient descent
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    
+    # not 100% sure what "compile" does, maybe just runs the gradient descent algorithm
+    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
+
+model = baseline_model()
+model.summary()
+x_train, x_test, y_train, y_test = train_test_split(xtr, ytr, test_size=0.2, random_state=42)
+label_encoder = LabelEncoder()
+label_encoder.fit(y_train)
+encoded_y = np_utils.to_categorical((label_encoder.transform(y_train)))
+label_encoder.fit(y_test)
+encoded_y_test = np_utils.to_categorical((label_encoder.transform(y_test)))
+estimator = model.fit(x_train, encoded_y, epochs=20, batch_size=64)
+print("Model Trained!")
+score = model.evaluate(x_test, encoded_y_test)
+print("")
+print("Accuracy = " + format(score[1]*100, '.2f') + "%")   # 92.62%
