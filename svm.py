9cd40c077bd2945b02f2b4efedd18c1297bdfd86
Keval Khara <kevalkhara95@gmail.com>
2018-04-29 20:03:36 -0400

    SVM

---
+++ svm.py
@@ -0,0 +1,21 @@
+from getEmbeddings import getEmbeddings
+import numpy as np
+from sklearn.svm import SVC
+
+xtr,xte,ytr,yte = getEmbeddings("datasets/train.csv")
+np.save('./xtr',xtr)
+np.save('./xte',xte)
+np.save('./ytr',ytr)
+np.save('./yte',yte)
+
+xtr = np.load('./xtr.npy')
+xte = np.load('./xte.npy')
+ytr = np.load('./ytr.npy')
+yte = np.load('./yte.npy')
+
+clf = SVC()
+clf.fit(xtr, ytr)
+y_pred = clf.predict(xte)
+m = yte.shape[0]
+n = (yte != y_pred).sum()
+print("Accuracy = " + format((m-n)/m*100, '.2f') + "%")   # 88.42%
